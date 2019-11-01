cfc626c1ca236e0833596d22f76d029a90087116
Martin Berg <martin.yngve.berg@gmail.com>
2019-11-01 16:01:44 +0100

    .

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using Tesseract;
 
 namespace PDFnamer
 {
@@ -12,7 +17,14 @@ namespace PDFnamer
         {
             Console.WriteLine("Test");
             Console.ReadKey();
+            Bitmap image = new Bitmap("C:\\Users\\SEQVQQ\\source\\repos\\PDFnamer\\test.png");
+            //Bitmap image = new Bitmap(@"test.png");
+            Console.WriteLine(image.Size.Width);
+            Console.ReadKey();
+            TesseractEngine engine = new TesseractEngine(@"./tessdata", "swe", EngineMode.Default);
+            Page results = engine.Process(image);
 
+            Console.WriteLine(results.ToString());
         }
     }
 }
